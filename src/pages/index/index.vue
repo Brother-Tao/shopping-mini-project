353c1b10de9c6eb84ef3b212d2808aeb4b8466c2
Tao <918534520@qq.com>
2023-11-07 21:03:23 +0800

    feat: 引入uni-ui

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <view class="index">首页</view>
+  <view class="index">
+    <uni-card title="基础卡片" extra="额外信息">
+      <text>这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text>
+    </uni-card></view
+  >
 </template>
 
 <style lang="scss">
