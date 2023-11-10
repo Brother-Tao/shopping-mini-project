6ea82de1a8b50dc4cc3453f617bc8b2400c20f9b
Tao <918534520@qq.com>
2023-11-10 20:29:46 +0800

    feat: 商品分类完成

---
+++ src/services/goods.ts
@@ -0,0 +1,10 @@
+import type { GoodsResult } from '@/types/goods'
+import { http } from '@/utils/http'
+
+export const gteGoodsDetailApi = (id: string) => {
+  return http<GoodsResult>({
+    method: 'GET',
+    url: '/goods',
+    data: { id },
+  })
+}
