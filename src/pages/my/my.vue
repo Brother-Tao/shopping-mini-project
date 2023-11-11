3bff277e1c80b6f7013ad550896f5bccc637f876
Tao <918534520@qq.com>
2023-11-11 21:35:40 +0800

    feat: 设置页分包预下载

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,32 +1,14 @@
-// src/pages/my/my.vue
-
-<script setup lang="ts">
-// 获取屏幕边界到安全区域距离
-const { safeAreaInsets } = uni.getSystemInfoSync()
-// 订单选项
-const orderTypes = [
-  { type: 1, text: '待付款', icon: 'icon-currency' },
-  { type: 2, text: '待发货', icon: 'icon-gift' },
-  { type: 3, text: '待收货', icon: 'icon-check' },
-  { type: 4, text: '待评价', icon: 'icon-comment' },
-]
-</script>
-
 <template>
-  <scroll-view class="viewport" scroll-y enable-back-to-top>
+  <scroll-view class="viewport" @scrolltolower="onScrolltolower" scroll-y enable-back-to-top>
     <!-- 个人资料 -->
     <view class="profile" :style="{ paddingTop: safeAreaInsets!.top + 'px' }">
       <!-- 情况1：已登录 -->
-      <view class="overview" v-if="false">
+      <view class="overview" v-if="memberStore.profile">
         <navigator url="/pagesMember/profile/profile" hover-class="none">
-          <image
-            class="avatar"
-            mode="aspectFill"
-            src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/avatar_3.jpg"
-          ></image>
+          <image class="avatar" mode="aspectFill" :src="memberStore.profile.avatar"></image>
         </navigator>
         <view class="meta">
-          <view class="nickname"> 黑马程序员 </view>
+          <view class="nickname">{{ memberStore.profile.nickname || '未设置昵称' }} </view>
           <navigator class="extra" url="/pagesMember/profile/profile" hover-class="none">
             <text class="update">更新头像昵称</text>
           </navigator>
@@ -80,10 +62,26 @@ const orderTypes = [
     </view>
     <!-- 猜你喜欢 -->
     <view class="guess">
-      <XtxGuess ref="guessRef" />
+      <CommonGuess ref="guessRef" />
     </view>
   </scroll-view>
 </template>
+
+<script setup lang="ts">
+import { useGuessList } from '@/composables/guess'
+import { useMemberStore } from '@/stores'
+const memberStore = useMemberStore()
+// 获取屏幕边界到安全区域距离
+const { safeAreaInsets } = uni.getSystemInfoSync()
+// 订单选项
+const orderTypes = [
+  { type: 1, text: '待付款', icon: 'icon-currency' },
+  { type: 2, text: '待发货', icon: 'icon-gift' },
+  { type: 3, text: '待收货', icon: 'icon-check' },
+  { type: 4, text: '待评价', icon: 'icon-comment' },
+]
+const { guessRef, onScrolltolower } = useGuessList()
+</script>
 
 <style lang="scss">
 page {
